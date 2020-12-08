790384b265c987e955f0584fcc87796d1f46e430
Aaron Carnahan <aaron.b.carnahan@outlook.com>
2020-12-07 20:36:20 -0500

    added edit functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -36,5 +36,64 @@ namespace RestaurantRater.Controllers
 
             return View(restaurant);
         }
+
+        // GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id) 
+        {
+            if (id == null) 
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null) 
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id) 
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Restaurant/Edit/{id}
+        // Get an id from the user
+        // Handle if the id is null
+        // Find a restaurant by that id
+        // If the restaurant doesn't exist
+        // Return the restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) 
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+			if (restaurant == null)
+			{
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+			}
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Edit/{id}
+        [HttpPost]
+        public ActionResult Edit(Restaurant restaurant) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.Entry(restaurant).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
     }
 }
